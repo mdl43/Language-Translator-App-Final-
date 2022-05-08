93c13dcb09fd2b86869f400a3a65ed5ca9794e10
mdl43 <43319650+mdl43@users.noreply.github.com>
2022-05-08 17:06:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+
+def intro():
+  print('Welcome to the Spanish and French translator app.\n\nPlease enter an English word and press the "Enter" key.')
+  print('\nType "done" at any time to exit')
+
+def exit():
+  print('\nThanks for using the translator app. Have a great day!')
+
+translations = {
+}
+
+with open("translations.csv", "r") as words:
+  reader = csv.DictReader(words, delimiter=",")
+  for line in reader:
+    english = line["English"].lower()
+    spanish = line["Spanish"].lower()
+    french = line["French"].lower()
+    translations[english] = [spanish, french]
+    
+
+
+
+done = False
+
+intro()
+
+while not done:
+  word = input("\nType an English word to translate: " ).lower()
+  
+  if word == "done":
+    done = True
+    exit()
+  elif word in translations:
+    print(f'\nSPANISH: {translations[word][0]}')
+    print(f'\nFRENCH: {translations[word][1]}')
+    print(f'\n...')
+  else:
+    print("\nTranslation is not known")
+    
